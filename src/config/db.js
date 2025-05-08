b66bada30bcbbf5133b50e362e4b96af7812a5cb
SilvestreG <silvestregines@disroot.org>
2025-05-09 00:40:02 +0200

    Cambios para que no se incluya .env ni node_modules en los commits a git. Se guardan las credenciales de conexión a la base de datos en .env para proteger datos sensibles.

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,35 @@
 //Conexión con MongoDB usando Mongoose
 // src/config/db.js
 
-const mongoose = require('mongoose'); //Se importa la librería moongose para facilitar la conexión y la interacción con MongoDB.
+const mongoose = require("mongoose"); //Se importa la librería moongose para facilitar la conexión y la interacción con MongoDB.
 
-const uri = "mongodb+srv://silvestregines:qG5gUTCwiyCqbGPG@backend-tienda.pb8ctut.mongodb.net/tiendaDB?retryWrites=true&w=majority&appName=backend-tienda"; //String de conexión a clúster de MongoDB en Atlas. Por orden incluye: usuario:contraseña@clusterAtlas/base_de_datos
-const clientOptions = { 
-  serverApi: { 
-    version: '1', 
-    strict: true, 
-    deprecationErrors: true 
-  } 
+//Cargar las variables de entorno
+require('dotenv').config( { path: './.env' });
+
+//Obtener la URI desde el archivo .env
+const uri = process.env.DB_URI; //String de conexión a clúster de MongoDB en Atlas. Por orden incluye: usuario:contraseña@clusterAtlas/base_de_datos
+
+const clientOptions = {
+  serverApi: {
+    version: "1",
+    strict: true, //Modo estricto en la comunicación con el servidor.
+    deprecationErrors: true,  //Aquí se fuerza a que muestre errores si se usan funcionalidades obsoletas (deprecated)
+  },
 };
 
-async function connectDB() { //Se declara coonectDB como función asíncrona
-  try { //Se inicia try para capturar posibles errores en la conexión
+async function connectDB() {
+  //Se declara coonectDB como función asíncrona
+  try {
+    //Se inicia try para capturar posibles errores en la conexión
     // Conecta a MongoDB
     await mongoose.connect(uri, clientOptions); //Conexión a MongoDB usando la uri y las clientOptions definidas anteriormente
-    // Opcional: hacer un ping para verificar la conexión  
+    // Opcional: hacer un ping para verificar la conexión
     await mongoose.connection.db.admin().command({ ping: 1 }); //Se comprueba si la conexión es exitosa haciendo un ping
     console.log("Conexión a MongoDB exitosa");
-  } catch (error) { //En caso de error se captura
+  } catch (error) {
+    //En caso de error en el bloque try se captura
     console.error("Error al conectar con MongoDB:", error); //Lo muestra en consola
-    process.exit(1); //Y se finaliza el proceso 
+    process.exit(1); //Y se finaliza el proceso
   }
 }
 
